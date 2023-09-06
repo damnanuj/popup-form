19feaa99e86fbeb1e2905de44b87a50120d54da8
Anuj <anujkrgupta21@gmail.com>
2023-09-06 18:18:33 +0530

    minor changes

--- script.js
+++ script.js
@@ -2,25 +2,17 @@
 
 
 // document.addEventListener("DOMContentLoaded", function () {
-//     const logBtn = document.querySelector(".btn");
-//     const formContainer = document.querySelector(".formContainer");
 
-//     logBtn.addEventListener("click", () => {
-//         formContainer.classList.toggle("hidelogin");
-//         console.log("harry");
-//     });
 // });
 
 
 const logBtn = document.querySelector(".btn");
 const formContainer = document.querySelector(".formContainer");
 const hider =document.querySelector(".xmark");
+
 logBtn.addEventListener("click", () => {
-    formContainer.classList.remove("hidelogin");
     formContainer.classList.add("popupShow")
 });
-
-// hider.addEventListener ("click", () => logBtn.click())
 
 hider.addEventListener("click", () => {
     formContainer.classList.remove("popupShow")
