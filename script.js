1204ab0c0652b7299e19725f12bf68936b9f265e
Anuj <anujkrgupta21@gmail.com>
2023-09-06 18:08:01 +0530

    added transition

--- script.js
+++ script.js
@@ -17,10 +17,11 @@ const formContainer = document.querySelector(".formContainer");
 const hider =document.querySelector(".xmark");
 logBtn.addEventListener("click", () => {
     formContainer.classList.remove("hidelogin");
+    formContainer.classList.add("popupShow")
 });
 
 // hider.addEventListener ("click", () => logBtn.click())
 
 hider.addEventListener("click", () => {
-    formContainer.classList.add("hidelogin")
+    formContainer.classList.remove("popupShow")
 })
